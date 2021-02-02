19cdbaf94be4f64037302a6f66d154dc89d986fc
Astley Santos <astley_394@hotmail.coml>
2021-02-02 18:07:58 +0100

    Finished Project + commented. Next play more with css and level up the project

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
-    $('.filter'.click(function(){
+    // Check if any selector with the class name filter was clicked
+    $('.filter').click(function(){
+        // This variable will contain the value of the clicked filter
         const value = $(this).attr('data-filter');
 
         if (value === 'all') {
+            // Will show all the values that don't contain '.all'
             $('.item').not('.' + value).show('1000');
         } else {
+            // Will hide all the values that don't contain its filter value
             $('.item').not('.' + value).hide('1000');
+            // Will show all the values that don't contain its filter value
             $('.item').filter('.' + value).show('1000');
         }
-    }))
+    })
 })
 
 $('.filter').click(function() {
+    // Will change state of the active element
     $(this).addClass('active').siblings().removeClass('active');
 })
